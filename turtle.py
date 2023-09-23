d0ccf7ef97893261ff35a277e38d0b709acd60ee
MahsaHemati <132953760+MahsaHematii@users.noreply.github.com>
2023-09-23 12:11:39 +0330

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,29 +1,26 @@
 import turtle
-import math
 
-colors = ["blue","red"]
-r = 20
-n = 3
-x = 0
-y = 0
 
-p = turtle.pen()
+p = turtle.Pen()
 p.shape("turtle")
+n = 3
 
-while True:
-    for j in range(len(colors)):
-        p.pencolor(colors[j])
-        for i in range(n):
-            if i == 0:
-                p.left((360-((n-2)*180/n))/2)
-                
-            else:
-                p.left(360/n)    
-            p.forward(2*r*math.sin(math.pi/n))
-        r += 10
-        n += 1
-        x += 10
-        p.penup()
-        p.goto(x, y)
-        p.pendown()
-        p.setheading(0)
+while n >= 3:
+    i = (n - 2) * 180/n
+    p.left(180 - i / 2)
+    
+    for j in range(n):
+        p.left(180 - i)
+        p.forward(70 * (1 + n / 10))
+
+    
+    p.right(180 - i / 2)
+    p.penup()
+    p.forward(12 * ( 1 + n / 10))
+    p.pendown()
+    
+
+    n += 1
+    
+
+turtle.done()
